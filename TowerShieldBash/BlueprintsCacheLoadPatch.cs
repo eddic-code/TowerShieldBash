515d2044ac1d5392d1a80bee135ca37f40424a1c
Eddic-Code <eddic.code@gmail.com>
2021-11-21 12:34:11 -0600

    Initial commit.

---
+++ TowerShieldBash/BlueprintsCacheLoadPatch.cs
@@ -0,0 +1,42 @@
+﻿// ReSharper disable UnusedMember.Global
+// ReSharper disable InconsistentNaming
+
+using HarmonyLib;
+using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.Items.Armors;
+using Kingmaker.Blueprints.Items.Shields;
+using Kingmaker.Blueprints.Items.Weapons;
+using Kingmaker.Blueprints.JsonSystem;
+
+namespace TowerShieldBash
+{
+    [HarmonyPatch(typeof(BlueprintsCache), "Load")]
+    internal static class BlueprintsCacheLoadPatch
+    {
+        private static BlueprintShieldType _towerShieldType;
+        private static BlueprintItemWeaponReference _heavyShieldWeaponRef;
+
+        private static BlueprintShieldType TowerShieldType => _towerShieldType ??= GetBlueprint<BlueprintShieldType>("5f0f4b6e480e7054b8592b5a8b55854a");
+
+        private static BlueprintItemWeaponReference HeavyShieldWeaponRef => _heavyShieldWeaponRef ??= GetBlueprint<BlueprintItemWeapon>("ff8047f887565284e93773b4a698c393")
+            .ToReference<BlueprintItemWeaponReference>();
+
+        public static void Postfix(ref SimpleBlueprint __result)
+        {
+            if (Main.Enabled && __result is BlueprintItemShield shield && shield.Type == TowerShieldType 
+                && !string.IsNullOrWhiteSpace(shield.m_DisplayNameText) && !shield.m_WeaponComponent.Equals(HeavyShieldWeaponRef))
+            {
+                shield.m_WeaponComponent = HeavyShieldWeaponRef;
+            }
+        }
+
+        private static T GetBlueprint<T>(string guid) where T : SimpleBlueprint 
+        {
+            var assetGuid = new BlueprintGuid(System.Guid.Parse(guid));
+            var asset = ResourcesLibrary.TryGetBlueprint(assetGuid) as T;
+            if (asset == null) { Main.Log($"ERROR: Could not load asset: {guid}"); }
+
+            return asset;
+        }
+    }
+}
---
+++ TowerShieldBash/Main.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using UnityModManagerNet;
+
+namespace TowerShieldBash
+{
+    internal static class Main
+    {
+        private static UnityModManager.ModEntry _modEntry;
+
+        public static bool Enabled;
+
+        public static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            _modEntry = modEntry;
+            modEntry.OnToggle = OnToggle;
+            Enabled = modEntry.Enabled;
+
+            var harmony = new Harmony(modEntry.Info.Id);
+            harmony.PatchAll();
+            return true;
+        }
+
+        public static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            Enabled = value;
+            return true;
+        }
+
+        public static void Log(string msg)
+        {
+            _modEntry.Logger.Log(msg);
+        }
+    }
+}
